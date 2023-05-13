b80c81745ccbf08e29f4ec917488136049613e6b
Gangbean <xbox2537@gmail.com>
2023-05-14 07:28:35 +0900

    (doc) initial set

---
+++ java/main/LeetcodeAlgoApplication.java
@@ -0,0 +1,13 @@
+package main;
+
+public class LeetcodeAlgoApplication {
+
+    public static final String INPUT_PATH = "/file";
+
+    public static final String PROBLEM_PATH = "/problem";
+
+    public static void main(String[] args) {
+
+    }
+
+}
